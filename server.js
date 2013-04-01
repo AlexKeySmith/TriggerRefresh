a25cc32bcfbb2caa1dbc9e56ebc20f663b9459d5
Alex Key <alexander.key@gmail.com>
2013-04-01 13:13:57 -0400

    initial experimental working version of openid authentication and socket.io connectiong

--- server.js
+++ server.js
@@ -87,8 +87,9 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('refresh', function (data) {
+    console.log("refresh: " + data);
+    socket.broadcast.emit("refresh")
   });
 });
 
