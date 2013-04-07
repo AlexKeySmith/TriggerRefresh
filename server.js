21bc523b37bd0ae459a16229eb6371fc092c6e00
Alex Key <alexander.key@gmail.com>
2013-04-07 16:36:01 -0400

    early half working prototype - it will now connect to logged in browsers and trigger a refresh, but will not do it a second time without logging in again (yet)

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.configure(function() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
-  app.use(express.static(__dirname + '/../../public'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 
